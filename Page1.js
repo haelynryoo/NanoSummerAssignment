a9b9154980471996fbc863042474cde131a42dd9
Haelyn <haelyn.ryoo@labmiddleschool.com>
2019-07-15 20:46:06 -0400

    Commit
    Idk just testing around

--- Page1.js
+++ Page1.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
   $(".GameInfoHide").click(function(){
-    $(".GameInfoHide").hide();
+    $(".welcomeParagraph").hide();
   });
   $(".GameInfoShow").click(function(){
-    $(".GameInfoHide").show();
+    $(".welcomeParagraph").show();
   });
 });

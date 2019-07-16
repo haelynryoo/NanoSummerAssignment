1512e8271a42a6b31325689c085e312218aa7091
Haelyn <haelyn.ryoo@labmiddleschool.com>
2019-07-15 20:12:16 -0400

    Commit
    Show and Hide button for game info button
     currently broken

---
+++ Page1.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $(".GameInfoHide").click(function(){
+    $(".GameInfoHide").hide();
+  });
+  $(".GameInfoShow").click(function(){
+    $(".GameInfoHide").show();
+  });
+});

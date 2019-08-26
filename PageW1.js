d845f577784c53f00b4feaaa857339bc33748905
Haelyn <haelyn.ryoo@labmiddleschool.com>
2019-08-26 19:11:01 -0400

    HELP!!
    i added console.logs and none of them are working. I dont know what i am doing wrong.

--- PageW1.js
+++ PageW1.js
@@ -2,41 +2,76 @@
 var LMLevel = 0
 var SJLevel = 0
 var LDLevel = 0
-var
-
 
 $(document).ready(function(){
-  $(".GameInfoHide").click(function(){
-    $(".welcomeParagraph").hide();
-  });
-  $(".GameInfoShow").click(function(){
-    $(".welcomeParagraph").show();
+  $("button").click(function(){
+    $("p").slideToggle();
   });
 });
-//LM role hide/show
 $(document).ready(function(){
-  $(".LMInfoHide").click(function(){
-    $(".LMParagraph").hide();
-  });
-  $(".LegMuscleTrait").click(function(){
-    $(".LMParagraph").show();
+  $("button").click(function(){
+    $("p").slideToggle();
   });
 });
-//Warrior role hide/show
 $(document).ready(function(){
-  $(".SJInfoHide").click(function(){
-    $(".SJParagraph").hide();
-  });
-  $(".StrongJawTrait").click(function(){
-    $(".SJParagraph").show();
+  $("button").click(function(){
+    $("p").slideToggle();
   });
 });
-//Hunters role hide/show
 $(document).ready(function(){
-  $(".LDInfoHide").click(function(){
-    $(".LDParagraph").hide();
-  });
-  $(".LoyaltyDeterminationTrait").click(function(){
-    $(".LDParagraph").show();
+  $("button").click(function(){
+    $("p").slideToggle();
   });
 });
+$(document).ready(function(){
+  $("button").click(function(){
+    $("p").slideToggle();
+  });
+});
+$(document).ready(function(){
+  $("button").click(function(){
+    $("p").slideToggle();
+  });
+});
+$(document).ready(function(){
+  $("button").click(function(){
+    $("p").slideToggle();
+  });
+});
+
+// $(document).ready(function(){
+//   $(".GameInfoHide").click(function(){
+//     $(".welcomeParagraph").hide();
+//   });
+//   $(".GameInfoShow").click(function(){
+//     $(".welcomeParagraph").show();
+//   });
+// });
+// //LM role hide/show
+// $(document).ready(function(){
+//   $(".LMInfoHide").click(function(){
+//     $(".LMParagraph").hide();
+//   });
+//   $(".LegMuscleTrait").click(function(){
+//     $(".LMParagraph").show();
+//   });
+// });
+//
+// //Warrior role hide/show
+// $(document).ready(function(){
+//   $(".SJInfoHide").click(function(){
+//     $(".SJParagraph").hide();
+//   });
+//   $(".StrongJawTrait").click(function(){
+//     $(".SJParagraph").show();
+//   });
+// });
+// //Hunters role hide/show
+// $(document).ready(function(){
+//   $(".LDInfoHide").click(function(){
+//     $(".LDParagraph").hide();
+//   });
+//   $(".LoyaltyDeterminationTrait").click(function(){
+//     $(".LDParagraph").show();
+//   });
+// });
--- PageW2.js
+++ PageW2.js
@@ -15,15 +15,22 @@ var LDLevel = 0;
 //LD Trait
 
 //find food trait
+$(document).ready(function(){
 $(".FindFood").click(function() {
+  console.log(hi)
 var findFoodRandom = Math.floor(Math.random() * 2);
+console.log(mathRandomFoodWorks);
 if (findFoodRandom = 0){
   energy = energy + 25;
+  console.log(energyPlus25);
 $(".energyCount").html("You currently have" + energy + "Energy Units ");
+console.log(updateEnergyParaGood);
 }
 else if (findFoodRandom = 1){
   energy = energy - 5;
+  console.log(energyMinus5);
   $(".energyCount").html("You currently have" + energy + "Energy Units ");
+console.log(updateEnergyParaBad);
 }
-
+});
 });
